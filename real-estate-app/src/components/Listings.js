db18b8c84d733a6fe8683be15cf8c8289994d66a
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-08-26 19:28:40 -0400

    working on listings area search area frame is done no styleing applied yet

--- real-estate-app/src/components/Listings.js
+++ real-estate-app/src/components/Listings.js
@@ -4,29 +4,32 @@ export default class Listings extends Component {
     render() {
         return (
             <section className="listings">
+
                 <section className="search-area">
                     <input type="text" name="search" />
-
                 </section>
 
 
                 <section className="sortby">
-                    <div>
-                        390 results found
-                    </div>
+                    <div> 390 results found</div>
                     <div className="sort-options">
                         <select name="sortby" className="sortby">
                             <option value="price-asc">Highest Price</option>
+                            <option value="price-asc">Lowest Price</option>
                         </select>
+                        <div className="view">
+                            <i className="fas fa-list"></i>
+                            <i className="fas fa-bars"></i>
+                        </div>
                     </div>
-
-
                 </section>
 
 
 
                 <section className="listings-results">
-
+                    <div className="listing">
+                        
+                    </div>
 
 
                 </section>
